e1202e0065961f2a65e7f42d89f4e7ac26b74f37
olejandr <sora.oleksandr@gmail.com>
2021-05-26 13:59:44 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,39 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health -=  damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health -=  damage;
+        if(this.health = 0){
+            return `${this.name} has died in act of combat`
+        }else{
+            return `${this.name} has received ${this.damage} points of damage`
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+
+}
 
 // Saxon
 class Saxon {}
